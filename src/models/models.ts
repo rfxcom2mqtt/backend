17332be63161b9c432d8314f3aef0abf2f40fec5
sguernion <gsylvain35@gmail.com>
2024-02-07 22:48:50 +0100

    save device state

--- src/models/models.ts
+++ src/models/models.ts
@@ -4,10 +4,63 @@ export interface KeyValue {
   [s: string]: any;
 }
 
-export class DeviceState {
-  id: string = "";
-  type: string = "";
-  subtype: string = "";
+export class DeviceEntity {
+  public manufacturer: string = "Rfxcom";
+  public via_device: string = "rfxcom2mqtt_bridge";
+  public identifiers: string[] = [];
+  public name: string = "";
+
+  constructor(
+    identifiers: string[] = [],
+    //public model: string = '',
+    name: string = "",
+  ) {
+    this.identifiers = identifiers;
+    this.name = name;
+  }
+}
+
+export class DeviceState extends DeviceEntity {
+  public id: string = "";
+  public type: string = "";
+  public subtype: string = "";
+  entities: string[] = [];
+  sensors: string[] = [];
+  switchs: string[] = [];
+
+  constructor(identifiers: string[], name: string) {
+    super(identifiers, name);
+  }
+}
+
+export class DeviceStateStore {
+  public state: DeviceState;
+
+  constructor(state: DeviceState) {
+    this.state = state;
+  }
+
+  getInfo() {
+    return new DeviceEntity(this.state.identifiers, this.state.name);
+  }
+
+  addEntity(entityId: string) {
+    if (!this.state.entities.includes(entityId)) {
+      this.state.entities.push(entityId);
+    }
+  }
+
+  addSensor(sensorId: string) {
+    if (!this.state.entities.includes(sensorId)) {
+      this.state.sensors.push(sensorId);
+    }
+  }
+
+  addSwitch(switchId: string) {
+    if (!this.state.entities.includes(switchId)) {
+      this.state.switchs.push(switchId);
+    }
+  }
 }
 
 export class EntityState {
@@ -20,17 +73,6 @@ export class BridgeInfo {
   coordinator: RfxcomInfo = new RfxcomInfo();
   version: string = "";
   logLevel: string = "";
-}
-
-export class DeviceEntity {
-  public manufacturer: string = "Rfxcom";
-  public via_device: string = "rfxcom2mqtt_bridge";
-
-  constructor(
-    public identifiers: string[] = [],
-    //public model: string = '',
-    public name: string = "",
-  ) {}
 }
 
 export class DeviceBridge {
