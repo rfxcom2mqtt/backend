ccbdfc7e2682cf4c6af48c6e8b0e3ceb186625d3
Sylvain Guernion <gsylvain35@gmail.com>
2024-02-09 19:22:56 +0100

    add dev configuration

--- src/api/Frontend.ts
+++ src/api/Frontend.ts
@@ -2,6 +2,8 @@ import { logger } from "../libs/logger";
 import { Router } from "express";
 import { Settings, SettingFrontend } from "../settings";
 import expressStaticGzip from "express-static-gzip";
+import serverStatic from "serve-static";
+import path from "path";
 import fs from "fs";
 // @ts-ignore
 import frontend from "@rfxcom2mqtt/frontend";
@@ -14,17 +16,25 @@ export default class Frontend {
     this.frontConf = conf.frontend;
     this.router = Router();
 
-    const staticFrontend = expressStaticGzip(frontend.getPath(), {
-      enableBrotli: true,
-      index: "index.html",
-      customCompressions: [
-        {
-          encodingName: "deflate",
-          fileExtension: "zz",
-        },
-      ],
-      orderPreference: ["br", "gz"],
-    });
+    let staticFrontend;
+    if(process.env.PROFILE === "development"){
+      logger.debug("display local developement frontend build");
+      const publicFiles = path.join(__dirname, "../../../frontend/build/");
+      this.listPublicFiles(publicFiles)
+      staticFrontend = serverStatic(publicFiles);
+    }else{
+      staticFrontend = expressStaticGzip(frontend.getPath(), {
+        enableBrotli: true,
+        index: "index.html",
+        customCompressions: [
+          {
+            encodingName: "deflate",
+            fileExtension: "zz",
+          },
+        ],
+        orderPreference: ["br", "gz"],
+      });
+    }
     this.router.use(staticFrontend);
   }
 
--- src/dev.ts
+++ src/dev.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 process.env["RFXCOM2MQTT_DATA"] = "./config/";
+process.env["PROFILE"] = "development";
 
 require("./index.ts");
